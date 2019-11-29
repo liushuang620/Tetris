638dfe5fc314288ce4b1589d003c28ce50658f3f
liushuang620 <liushuang_620@163.com>
2019-11-29 22:12:27 +0800

    俄罗斯方块0.0

---
+++ Assets/Scripts/GameControler.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControler : MonoBehaviour
+{
+    public static int width = 10;
+    public static int height = 17;
+    public static Transform[,] grid = new Transform[width, height];
+
+    /*
+     * 判断边界
+     */
+    public static bool InsideBorder(Transform block)
+    {
+        return block.position.y >= 0 && block.position.x >= 0 && block.position.x <= 9;
+    }
+
+    public static bool IsValidPosition(GameObject shape)
+    {
+        foreach (Transform block in shape.transform)
+        {
+            int x = (int)block.position.x;
+            int y = (int)block.position.y;
+            if (!InsideBorder(block))
+            {
+                return false;
+            }
+            if (grid[x, y] != null && grid[x, y].parent != shape.transform)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static bool IsFullRow(int y)
+    {
+        for(int x = 0; x < 10; x++)
+        {
+            if(grid[x, y] == null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void DestoryFullRow(int y)
+    {
+        for(int x = 0; x < 10; x++)
+        {
+            Destroy(grid[x, y].gameObject);
+            grid[x, y] = null;
+        }
+    }
+
+    public static void MoveDownRow(int y)
+    {
+        for(int x = 0; x < 10; x++)
+        {
+            if(grid[x, y] != null)
+            {
+                grid[x, y].position += Vector3.down;
+            }
+        }
+    }
+
+    public static void MoveDownRowAbove(int y)
+    {
+        for(int i = y; i < 17; i++)
+        {
+            MoveDownRow(i);
+        }
+    }
+}
---
+++ Assets/Scripts/ShapeMove.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeMove : MonoBehaviour
+{
+    public GameObject [] shapes;   
+    public float moveSpeed = 0.5f;
+    private float internalTime = 0f;
+
+    private GameObject newShape;
+
+    void Start()
+    {
+        newShape = GenerateShape();
+    }
+
+    void Update()
+    {
+        if (internalTime >= moveSpeed)
+        {
+            newShape.transform.position += Vector3.down;
+            if (!GameControler.IsValidPosition(newShape))
+            {
+                newShape.transform.position += Vector3.up;
+                foreach (Transform block in newShape.transform)
+                {
+                    int x = (int)block.position.x;
+                    int y = (int)block.position.y;
+                    GameControler.grid[x, y] = block;
+                }
+                for(int y = 0; y < 17; y++)
+                {
+                    if (GameControler.IsFullRow(y))
+                    {
+                        GameControler.DestoryFullRow(y);
+                        GameControler.MoveDownRowAbove(y);
+                    }
+                }
+                newShape = GenerateShape();
+                moveSpeed = 0.5f;
+            }
+            internalTime = 0f;
+        }
+        else
+        {
+            internalTime += Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            newShape.transform.position += Vector3.right;
+            if (!GameControler.IsValidPosition(newShape))
+            {
+                newShape.transform.position -= Vector3.right;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            newShape.transform.position += Vector3.left;
+            if (!GameControler.IsValidPosition(newShape))
+            {
+                newShape.transform.position -= Vector3.left;
+            }
+        }else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            moveSpeed /= 10;
+        }
+    }
+
+    /*
+     * 随机生成方块
+     */
+    GameObject GenerateShape()
+    {
+        int i = Random.Range(0, shapes.Length);
+        return Instantiate(shapes[i], transform.position, transform.rotation);
+    }
+}
